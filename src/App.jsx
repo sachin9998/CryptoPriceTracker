25e3afad2dab11b0ad16d92d48f4472966b4b868
Sachin Alam <sachinalam4216@gmail.com>
2024-07-08 21:35:44 +0530

    added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Coin from "./pages/Coin/Coin";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 	return (
@@ -13,6 +14,8 @@ function App() {
 
 				<Route path="/coin/:coinId" element={<Coin />} />
 			</Routes>
+
+			<Footer />
 		</div>
 	);
 }
