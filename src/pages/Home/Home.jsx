ecf4f76964bd8350e5f04fd7aace7fb4db7f3613
Sachin Alam <sachinalam4216@gmail.com>
2024-07-08 17:45:45 +0530

    working on homepage

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import "./Home.css";
+
+const Home = () => {
+	return (
+		<div className="home">
+			<div className="hero">
+				<h1>
+					Largest <br /> Crypto Marketplace
+				</h1>
+
+				<p>
+					Welcome to the world's largest cryptocurrency marketplace. Sign up to
+					explore more about cryptos.
+				</p>
+
+				{/* Search Crypto Form */}
+				<form>
+					<input type="text" placeholder="Search crypto." />
+
+					<button type="submit">Search</button>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
