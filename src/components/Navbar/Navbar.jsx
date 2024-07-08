2e425772e7e6c6f85c10e3bb686b0aa9ab31247e
Sachin Alam <sachinalam4216@gmail.com>
2024-07-08 20:54:19 +0530

    Hero Section done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,45 @@
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import arrowIcon from "../../assets/arrow_icon.png";
+import { useContext } from "react";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
+	const { setCurrency } = useContext(CoinContext);
+
+	const handleCurrency = (e) => {
+		switch (e.target.value) {
+			case "usd": {
+				setCurrency({
+					name: "usd",
+					symbol: "$",
+				});
+				break;
+			}
+			case "inr": {
+				setCurrency({
+					name: "inr",
+					symbol: "₹",
+				});
+				break;
+			}
+			case "eur": {
+				setCurrency({
+					name: "eur",
+					symbol: "€",
+				});
+				break;
+			}
+			default: {
+				setCurrency({
+					name: "usd",
+					symbol: "$",
+				});
+				break;
+			}
+		}
+	};
+
 	return (
 		<div className="navbar">
 			{/* Logo */}
@@ -19,7 +56,7 @@ const Navbar = () => {
 
 			{/* Currency Dropdonw Menu */}
 			<div className="nav-right">
-				<select>
+				<select onChange={handleCurrency}>
 					<option value="usd">USD</option>
 					<option value="euro">Euro</option>
 					<option value="inr">INR</option>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,16 @@
+import { useContext, useEffect } from "react";
 import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
+import { useState } from "react";
 
 const Home = () => {
+	const { allCoin, currency } = useContext(CoinContext);
+	const [displayCoin, setDisplayCoin] = useState([]);
+
+	useEffect(() => {
+		setDisplayCoin(allCoin);
+	}, [allCoin]);
+
 	return (
 		<div className="home">
 			<div className="hero">
@@ -19,6 +29,41 @@ const Home = () => {
 
 					<button type="submit">Search</button>
 				</form>
+			</div>
+
+			<div className="crypto-table">
+				<div className="table-layout">
+					<p>#</p>
+					<p>Coins</p>
+					<p>Price</p>
+					<p style={{ textAlign: "center" }}>24 Hour Change</p>
+					<p className="market-cap">Market Cap</p>
+				</div>
+
+				{displayCoin.slice(0, 10).map((item) => {
+					return (
+						<div key={item.id} className="table-layout">
+							<p>{item.market_cap_rank}</p>
+
+							<div>
+								<img src={item.image} alt="" />
+								<p> {`${item.name} - ${item.symbol}`}</p>
+							</div>
+
+							<p>{`${currency.symbol} ${item.current_price.toLocaleString()}`}</p>
+
+							<p
+								className={
+									item.price_change_percentage_24h > 0 ? "green" : "red"
+								}
+							>
+								{`${Math.floor(item.price_change_percentage_24h * 100) / 100} %`}{" "}
+							</p>
+
+							<p className="market-cap">{`${currency.symbol} ${item.market_cap.toLocaleString()}`}</p>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
